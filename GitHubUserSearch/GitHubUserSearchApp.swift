cb6505cdb6c1d8053b5ac4f21fd6ae29e0625344
LukaszDziwosz <35117633+LukaszDziwosz@users.noreply.github.com>
2021-09-17 13:51:56 +0200

    Initial Commit

---
+++ GitHubUserSearch/GitHubUserSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubUserSearchApp.swift
+//  GitHubUserSearch
+//
+//  Created by Lukasz Dziwosz on 17/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct GitHubUserSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
